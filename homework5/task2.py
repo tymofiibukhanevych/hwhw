5ed729774c0a7430a83af604e97a9ba21b80b543
tymofii <tymofii10@gmail.com>
2023-10-14 11:12:45 +0300

    homework 5 in progress

---
+++ homework5/task2.py
@@ -0,0 +1,5 @@
+my_name = input("Please enter your name: ")
+print("Hello, " + my_name )
+
+my_age = input("Please enter your age: ")
+print("Hello, " + my_name + "! You are " + my_age + " years old." )
