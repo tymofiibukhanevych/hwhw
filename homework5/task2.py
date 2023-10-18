b876ba384424b23f72334cfe88d647e5524958df
tymofii <tymofii10@gmail.com>
2023-10-18 13:44:41 +0300

    homework 5 in progress

--- homework5/task2.py
+++ homework5/task2.py
@@ -1,5 +1,6 @@
 my_name = input("Please enter your name: ")
-print("Hello, " + my_name )
 
-my_age = input("Please enter your age: ")
-print("Hello, " + my_name + "! You are " + my_age + " years old." )
+my_age = int(input("Please enter your age: "))
+
+next_age = my_age + 1
+print(f"Hello, {my_name}, on your next birthday you'll be {next_age} years old.  ")
